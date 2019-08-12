c82bbbd47ae66e535ee3561a2a92c9ab930dcb33
Anic Fraefel <anicfraefel@gmail.com>
2019-08-12 17:20:35 +0200

    solved the letter game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,33 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = 10.times.map { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @word = params[:word]
+    @letters = params[:letters]
+    @answer = 'You lose'
+    if same?(@word, @letters) && english_word?(@word)
+      @answer = 'Well done!'
+    elsif same?(@word, @letters)
+      @answer = 'Not an English word'
+    elsif english_word?(@word)
+      @answer = 'English word, but not valid to the grid'
+    else
+      @answer
+    end
+  end
+end
+
+def same?(word, letters)
+  word_array = word.upcase.split('')
+  word_array.all? { |letter| word_array.count(letter) <= letters.count(letter) }
+end
+
+def english_word?(word)
+  response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+  json = JSON.parse(response.read)
+  json['found']
+end
